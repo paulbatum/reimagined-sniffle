f221c8e07c752c695e871b92a56d1395cf396a52
Alex Karcher <alkarche@microsoft.com>
2020-01-17 14:56:39 -0800

    Sentiment Analysis!

--- CreateRating/index.js
+++ CreateRating/index.js
@@ -67,18 +67,83 @@ module.exports = async function (context, req) {
     }
     var locationNameValue = req.query.locationName || (req.body && req.body.locationName)
     var userNotesValue = req.query.userNotes || (req.body && req.body.userNotes)
-    var outputSniffledDoc = JSON.stringify({
-        userId: userIdValue,
-        productId: productIdValue,
-        timestamp: Date.now(),
-        rating: ratingValue,
-        locationName: locationNameValue,
-        userNotes: userNotesValue
-      });
-    context.bindings.outputSniffledDoc = outputSniffledDoc;
 
-    context.res = {
-        status: 200,
-        body: outputSniffledDoc
+    //Send userNotes through sentiment Analysis
+
+var sentimentScore; 
+
+    'use strict';
+
+let https = require ('https');
+
+subscription_key = "bca96e65e74b4ddd846523adb2c2db6f";
+endpoint = "https://westus.api.cognitive.microsoft.com/";
+
+let path = '/text/analytics/v2.1/sentiment';
+
+let response_handler = function (response) {
+    let body = '';
+    response.on('data', function (d) {
+        body += d;
+    });
+    response.on('end', function () {
+        let body_ = JSON.parse(body);
+        let body__ = JSON.stringify(body_, null, '  ');
+        console.log(body__);
+
+        console.log("debugging docs");
+        console.log(sentimentScore);    
+        
+        sentimentScore = body_.documents[0].score;
+        console.log.metric("Sentiment", sentimentScore);
+
+            var outputSniffledDoc = JSON.stringify({
+                userId: userIdValue,
+                productId: productIdValue,
+                timestamp: Date.now(),
+                rating: ratingValue,
+                locationName: locationNameValue,
+                userNotes: userNotesValue,
+                sentimentScore: sentimentScore
+              });
+            context.bindings.outputSniffledDoc = outputSniffledDoc;
+        
+            context.res = {
+                status: 200,
+                body: outputSniffledDoc
+            };
+    
+    
+    
+    });
+    response.on('error', function (e) {
+        console.log('Error: ' + e.message);
+    });
+};
+
+let get_sentiments = function (documents) {
+    let body = JSON.stringify(documents);
+
+    let request_params = {
+        method: 'POST',
+        hostname: (new URL(endpoint)).hostname,
+        path: path,
+        headers: {
+            'Ocp-Apim-Subscription-Key': subscription_key,
+        }
     };
+
+    let req = https.request(request_params, response_handler);
+    req.write(body);
+    req.end();
+}
+
+let documents = {
+    'documents': [
+        { 'id': '1', 'language': 'en', 'text': userNotesValue}
+    ]
+};
+
+get_sentiments(documents);
+
 };
