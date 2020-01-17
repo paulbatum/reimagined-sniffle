9e732995667f763667b38af3c19a503ff4b0ae58
pragnagopa <pgopa@microsoft.com>
2020-01-17 11:59:59 -0800

    Add receipts

--- SalesEventTrigger/index.js
+++ SalesEventTrigger/index.js
@@ -1,8 +1,36 @@
+var request = require('request-promise');
 module.exports = async function (context, eventHubMessages) {
     context.log(`JavaScript eventhub trigger function called for message array ${eventHubMessages}`);
-    
+
     eventHubMessages.forEach((message, index) => {
         context.log(`Processed message ${message}`);
+        var parsedMessage = JSON.parse(message);
+        var outReceipt = {
+            "Store": parsedMessage.store,
+            "SalesNumber": parsedMessage.SalesNumber,
+            "TotalCost": parsedMessage.totalCost,
+            "Items": parsedMessage.totalItems,
+            "SalesDate": parsedMessage.salesDate
+        }
+        if (parsedMessage.receiptUrl) {
+            if (parsedMessage.totalCost >= 100) {
+                try {
+                    var receipt = await request.get(parsedMessage.receiptUrl);
+                    var receiptBuffer = new Buffer(receipt);
+                    var encodedReceipt = receiptBuffer.toString('base64');
+                    outReceipt.ReceiptImage = encodedReceipt;
+                    context.bindings.highValueReceiptOutputBlob = outReceipt;
+                }
+                catch
+                {
+                    context.log(`Did not find reciept at ${parsedMessage.receiptUrl}`);
+                }
+            }
+            else
+            {
+                context.bindings.receiptOutputBlob = outReceipt;
+            }
+        }
     });
-    context.bindings.salesOutput = eventHubMessages; 
+    context.bindings.salesOutput = eventHubMessages;
 };
